854fecb23927dbaad9b10261c19b3d15875c2cd3
Lochlann O Neill <lochlannjoneill@gmail.com>
2022-10-09 18:35:00 +0100

    cleanup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into director(directorId, fname, lname, stillActive) values
-    (1, 'Lochlann', 'ONeill', true),
+    (1, 'Lochlann', 'O''Neill', true),
     (2, 'Cliona', 'McGuane', true),
     (3, 'Sean', 'McSweeney', false);
 
@@ -8,5 +8,4 @@ insert into movie(movieId, title, releaseDate, earnings, directorId) values
     (2, 'The Last Samurai', 'January 9, 2004', 456800000.0, 1),
     (3, 'Kung Fu Panda', 'July 4, 2008', 631700000.0, 2),
     (4, 'Ace Ventura: Pet Detective', 'February 4, 1994', 107200000.0, 3),
-    (5, 'Dumb and Dumber', 'Februaru 3, 1995', 247000000.0, 2),
-    (6, 'The Lion King', 'October 21, 1994', 1656943394.0, 1);
+    (5, 'Dumb and Dumber', 'Februaru 3, 1995', 247000000.0, 2);
